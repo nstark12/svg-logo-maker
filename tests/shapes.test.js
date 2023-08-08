f6228b3920e14d29f44bd08de63a3e4b1da0de9b
Marketing Director <nicolestark1103@gmail.com>
2023-08-08 11:08:59 -0500

    jest tests passed

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -12,3 +12,27 @@ describe('Circle Class', () => {
         expect(shape.render()).toBe(`<circle cx="150" cy="115" r="80" fill="${color}" />`)
     })
 })
+
+// test Square class
+describe('Square Class', () => {
+    it('renders correctly', () => {
+        const shape = new Square()
+
+        let color = ('pink')
+        shape.setColor(color)
+
+        expect(shape.render()).toBe(`<rect x="50" width="160" height="160" fill="${color}" />`)
+    })
+})
+
+// test Triangle class
+describe('Triangle Class', () => {
+    it('renders correctly', () => {
+        const shape = new Triangle()
+
+        let color = ('pink')
+        shape.setColor(color)
+
+        expect(shape.render()).toBe(`<polygon points="150, 18 244, 182 56, 182" fill="${color}" />`)
+    })
+})
